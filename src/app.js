491cae93770848bcb735ce95a308d11ee817f364
Noemi Soares <142548973+noemisoares@users.noreply.github.com>
2026-01-01 15:08:43 +0000

    Configuração do servidor Express e frontend básico

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+module.exports = app;
